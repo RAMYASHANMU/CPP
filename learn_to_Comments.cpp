fce526ab2611c8625b4a7f298227beae7a53af5d
Ramya S <43613038+RAMYASHANMU@users.noreply.github.com>
2022-04-10 20:01:15 +0530

    Create learn_to_Comments.cpp

---
+++ learn_to_Comments.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+//Comment the part that outputs b
+void comment(int a,int b,int c)
+{
+    cout<<a<<endl;
+   // cout<<b<<endl;
+   /*This is a comment*/
+//This is also a comment
+    cout<<c<<endl;
+}
