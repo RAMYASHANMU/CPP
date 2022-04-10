9e2487320c0ef4b09ca3298028900dc934c46394
Ramya S <43613038+RAMYASHANMU@users.noreply.github.com>
2022-04-10 19:29:54 +0530

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+class Solution{
+    public:
+    void helloWorld()
+    {
+    
+        //Type below cout<<"Hello World"; 
+        //cout will output to console
+        cout<<"Hello World"<<endl;
+        
+    }
+};
